3602a891f044c6d597ef874f349156f4dd3d4834
olPeePaw <chris.lovett@outlook.com>
2019-11-03 21:27:32 -0500

    Misspelled package name

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ curl -fsSL https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
 
-yum install -y docker-composer tcpdump iperf iperf3
+yum install -y docker-compose tcpdump iperf iperf3
 
 docker-compose up -d
