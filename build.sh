e9848d63d2b8c5e28510aabc8a961aa4399eb783
mcnc-clovett <clovett@mcnc.org>
2019-11-04 10:53:06 -0500

    Added iperf to systemd

--- build.sh
+++ build.sh
@@ -7,6 +7,17 @@ curl -fsSL https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
 
-yum install -y docker-compose tcpdump iperf iperf3
+yum install -y iperf iperf3
 
+adduser iperf -s /sbin/nologin
+cp ./systemd/* /etc/systemd/system/
+systemctl daemon-reload
+systemctl start iperf
+systemctl start iperf3
+systemctl enable iperf
+systemctl enable iperf3
+
+yum install -y tcpdump wireshark
+
+yum install -y docker-compose
 docker-compose up -d
