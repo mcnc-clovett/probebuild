34a0845ee8a897a81e4d1b3ef4ab3a53a773f13b
mcnc-clovett <clovett@mcnc.org>
2019-11-06 10:36:00 -0500

    Added httpd, index page for tools and firewall rules for all tools

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ curl -fsSL https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
 
-yum install -y iperf iperf3
+yum install -y tcpdump wireshark iperf iperf3 httpd
 
 adduser iperf -s /sbin/nologin
 cp ./systemd/* /etc/systemd/system/
@@ -17,7 +17,33 @@ systemctl start iperf3
 systemctl enable iperf
 systemctl enable iperf3
 
-yum install -y tcpdump wireshark
+PROBEIP=`hostname -i` cat <<EOF >/var/www/html/index.html
+<HTML>
+
+<HEAD>
+If the hyperlinks below do not work, go to address associated beneath.<br><br>
+</HEAD>
+
+<BODY>
+
+<a href="#" onclick="javascript:window.location.port=8081">Smokeping</a><br>
+http://$PROBEIP:8081/smokeping/sm.cgi<br><br>
+
+<a href="#" onclick="javascript:window.location.port=8080">Cacti</a><br>
+http://$PROBEIP:8080/cacti/<br><br>
+
+<a href="#" onclick="javascript:window.location.port=3000">NTop</a><br>
+http://$PROBEIP:3000
+
+</BODY>
+
+</HTML>
+EOF
 
 yum install -y docker-compose
 docker-compose up -d
+
+firewall-cmd --add-service=http --add-service=https
+firewall-cmd --add-port=3000/tcp --add-port=3333/tcp --add-port=5001/tcp --add-port=5001/udp \
+    --add-port=5201/tcp --add-port=5201/udp
+firewall-cmd --runtime-to-permanent
