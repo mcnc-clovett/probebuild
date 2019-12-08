5e1b8c35cc3964949f1af3189c14cc6afb0170f1
mcnc-clovett <57356081+mcnc-clovett@users.noreply.github.com>
2019-12-07 22:27:43 -0500

    Still forgot the s in https

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ http://$PROBEIP:8080<br><br>
 <a href="http://$PROBEIP:3000" onclick="javascript:window.location.port=3000">NTop</a><br>
 http://$PROBEIP:3000<br><br>
 
-<a href="http://$PROBEIP:9090" onclick="javascript:window.location.port=9090">Cockpit</a><br>
+<a href="https://$PROBEIP:9090" onclick="javascript:window.location.port=9090">Cockpit</a><br>
 https://$PROBEIP:9090
 
 </BODY>
