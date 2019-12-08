4ecf4c74e4c7342031f46449a322348776939280
clovett <clovett@mcnc.org>
2019-12-07 22:18:24 -0500

    Added Cockpit to main page

--- build.sh
+++ build.sh
@@ -49,6 +49,9 @@ http://$PROBEIP:8080<br><br>
 <a href="http://$PROBEIP:3000" onclick="javascript:window.location.port=3000">NTop</a><br>
 http://$PROBEIP:3000
 
+<a href="http://$PROBEIP:9090" onclick="javascript:window.location.port=9090">Cockpit</a><br>
+http://$PROBEIP:9090
+
 </BODY>
 
 </HTML>
