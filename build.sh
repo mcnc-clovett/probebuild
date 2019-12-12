70f9a61f61efbb03a89067bd6b663254fff884c3
mcnc-clovett <57356081+mcnc-clovett@users.noreply.github.com>
2019-12-12 09:50:36 -0500

    Added documentation and turned off lid suspend

--- build.sh
+++ build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+# Add Zscaler SSL certificate for those using SSL decryption
 cp ./ZscalerRootCertificate-2048-SHA256.crt /etc/pki/ca-trust/source/anchors/
-
 update-ca-trust
 
+# Set the Laptop Lid Switch handle to ignore so that the probe stays awake
+sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+
+# Install EPEL for CentOS
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
+# Install docker using official install script
 curl -fsSL https://get.docker.com/ | sh
 
+# Install other supporting applications
 yum install -y tcpdump wireshark iperf iperf3 httpd cockpit cockpit-docker docker-compose
 
+# Allow access to probe tools through the local firewall
 firewall-cmd --add-service=http --add-service=https --add-service=cockpit
 firewall-cmd --add-port=3000/tcp --add-port=3333/tcp --add-port=5001/tcp \
     --add-port=5001/udp --add-port=5201/tcp --add-port=5201/udp
 firewall-cmd --runtime-to-permanent
 
+# Add iperf service files and enable/start all services
 adduser iperf -s /sbin/nologin
 cp ./systemd/* /etc/systemd/system/
 systemctl start httpd
@@ -29,6 +37,7 @@ systemctl start iperf3
 systemctl enable iperf
 systemctl enable iperf3
 
+# Create homepage for web-based tools
 PROBEIP=$(hostname -i) 
 
 cat <<EOF >/var/www/html/index.html
@@ -57,4 +66,5 @@ https://$PROBEIP:9090
 </HTML>
 EOF
 
+# Download and start docker containers
 docker-compose up -d
